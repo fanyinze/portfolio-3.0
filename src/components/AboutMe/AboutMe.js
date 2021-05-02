e0fcbfffd404b883747d5544282b3902e1968e05
Yinze Fan <fanyinze@gmail.com>
2021-05-02 17:23:40 -0400

    master: update skills

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -3,9 +3,9 @@ import SkillBar from "./SkillBar";
 import resume from "../NavBar/assets/resume.docx";
 
 const skillData = [
-  { category: "React", bgcolor: "#f9ab48", percentage: 90 },
+  { category: "Java", bgcolor: "#f9ab48", percentage: 90 },
+  { category: "React", bgcolor: "#f9ab48", percentage: 80 },
   { category: "JavaScript", bgcolor: "#f9ab48", percentage: 80 },
-  { category: "Java", bgcolor: "#f9ab48", percentage: 80 },
   { category: "HTML/CSS", bgcolor: "#f9ab48", percentage: 75 },
   { category: "Node.js", bgcolor: "#f9ab48", percentage: 70 },
   { category: "Python", bgcolor: "#f9ab48", percentage: 70 },
